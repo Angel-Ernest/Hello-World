ebc28182be6f494ce245765b5fdc3a98bc0e912f
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-03-04 23:40:51 +0000

    I advanced a lot

--- TextAdventure.py
+++ TextAdventure.py
@@ -1,3 +1,22 @@
+import time
+
+#The questions
 print("Hello adventurer! I will be your guide in this adventure that you will experience. But first, let's do a few questions.")
-name = input("What's your name?")
+name = input("What's your name? ")
 print("Ok," , name)
+fear = input("What do you fear? ")
+print("That's very scary!")
+hero = input("What's your favorite hero? ")
+print("I also like",hero,"!")
+country = input("Where are you from? ")
+print("I would like to visit", country)
+power = ("What power would you like to have? ")
+print("Interesting!")
+supername = input("And finally: if you were a superhero, what would bee your name? ")
+print("Ok,", supername)
+print("Now, let's make your story!")
+time.sleep(1)
+print("Creating story...")
+time.sleep(3)
+#The story
+print("In a lost village, there was a kid called", name, ".", name , "feared" , fear , ". But one day he fought against it and won.", hero , "Appeared and reawarded", name, "with something incredible,", hero , "gifted", name , "a superpower! Out of nowhere," , name, "recieved had" , power , )
