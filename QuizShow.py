ef1ebb536401666f323d44a5de1734f6f9829137
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-03-18 21:26:09 +0000

    Function ready

--- QuizShow.py
+++ QuizShow.py
@@ -2,36 +2,39 @@ import time
 
 score = 0
 
+def check(answer):
+    global score
+    if answer == "Cell":
+        print("Correct")
+        score += 1
+        return 1
+    elif answer == "acute":        
+        print("Correct")
+        score += 1
+        return 1
+    elif answer == "9.8":
+        print("Correct")
+        score += 1
+        return 1
+    else:
+        print("Wrong")
+        return 0
+
 print("Welcome to the Math-science quiz (easy)! Let's see what are you capable of!")
 
 R1 = input("What is the basic unit of life?")
-
-if R1 == "Cell":
-    print("Correct")
-    score += 1
-else:
-    print("Wrong")
+      
+check(R1)
 
 time.sleep(1)
 
 R2 = input("An equilateral triangle has _______ angles")
 
-if R2 == "acute":
-    print("Correct")
-    score += 1
-else:
-    print("Wrong")
+check(R2)
 
 time.sleep(1)
 
 R3 = input ("The gravitational pull of the gravity in Earth is around (to the nearest tenth) ____ m/s")
-
-if R3 == "9.8":
-    print("Correct")
-    score += 1
-else:
-    print("Wrong")
-
-time.sleep(1)
+check(R3)
 
 print("Your score is," , score)
