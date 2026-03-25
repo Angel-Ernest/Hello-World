95d05c2e83e10ea216af8cad2738db1b0bea00ed
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-03-25 21:37:15 +0000

    I finished calculator :)

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,12 @@
-print("Pcalculator, basic calculator for basic caclculations")
- Z1 = input("First number: ")
+print("Scalculator, basic calculator for basic caclculations")
+Z1 = input("First number: ")
+Z2 = input("Second number: ")
+W1 = input("What do you want to add, subtract, multiply, or divide? ")
+if W1 == "add":
+    print(float(Z1) + float(Z2))
+elif W1 == "subtract":
+    print(float(Z1) - float(Z2))
+elif W1 == "multiply":
+    print(float(Z1) * float(Z2))
+elif W1 == "divide":
+    print(float(Z1) / float(Z2))
