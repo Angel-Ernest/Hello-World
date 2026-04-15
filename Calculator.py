5058ecd20363d5a46efd4e48c868b788b0ad50e0
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-04-15 21:27:58 +0000

    I finished calculator

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,18 @@
+import sys
+
 print("Scalculator, basic calculator for basic caclculations")
-Z1 = input("First number: ")
-Z2 = input("Second number: ")
+try:
+   Z1 = int(input("First number: "))
+except ValueError:
+    print("Please enter a valid number")
+    sys.exit(0)
+
+try:
+    Z2 = int(input("Second number: "))
+except ValueError:
+    print("Please enter a valid number")
+    sys.exit(0)
+
 W1 = input("What do you want to add, subtract, multiply, or divide? ")
 if W1 == "add":
     print(float(Z1) + float(Z2))
@@ -10,3 +22,5 @@ elif W1 == "multiply":
     print(float(Z1) * float(Z2))
 elif W1 == "divide":
     print(float(Z1) / float(Z2))
+else :
+    print("Please enter a valid operation")
