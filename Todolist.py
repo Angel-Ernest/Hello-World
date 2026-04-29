75cd5725e246361016253a56e474e8c281e12542
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-04-29 21:43:59 +0000

    I started To-do List

---
+++ Todolist.py
@@ -0,0 +1,21 @@
+import time
+
+mylist = ["Do work", "Clean my room", "Eat something"]
+
+print("Welcome to your To Do List!")
+time.sleep(1)
+
+print("Options\n1. Display tasks\n2. Add a task")
+time.sleep(0.5)
+
+while True:
+  A1 = input("Choose an option: ")
+
+  if A1 == "1":
+     print("Here is your current list: ")
+     print(mylist)
+    elif: A1 == "2":
+     
+     
+     
+ 
