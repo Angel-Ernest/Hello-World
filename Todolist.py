4d0e3c354c7cb1893ae11dda153e8a08dd6187fd
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-04-29 21:59:00 +0000

    I advance in the To-Do List

--- Todolist.py
+++ Todolist.py
@@ -5,17 +5,24 @@ mylist = ["Do work", "Clean my room", "Eat something"]
 print("Welcome to your To Do List!")
 time.sleep(1)
 
-print("Options\n1. Display tasks\n2. Add a task")
+print("Options\n1. Add a task\n2. Remove a task\n3. Complete a task\n4. Display tasks\n5. Exit")
 time.sleep(0.5)
 
 while True:
   A1 = input("Choose an option: ")
 
-  if A1 == "1":
-     print("Here is your current list: ")
-     print(mylist)
-    elif: A1 == "2":
-     
+  if A1 == "4":
+     for i, x in enumerate(mylist, start=1):
+       print(f"{i}. {x}")
+  elif A1 == "1":
+    NW = input("Add a task: ")
+    mylist.append(NW)
+  elif A1 == "2":
+    RW = int(input("Enter the number of the task to remove: ")) - 1
+    if 0 <= RW < len(mylist):
+      mylist.pop(RW)
+    else:
+      print("Invalid task number.")
      
      
  
