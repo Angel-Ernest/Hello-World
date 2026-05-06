3fa43378bf54ab940d5bcf20c3afd607a1aaeabd
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-05-06 21:59:32 +0000

    I finished To Do List

--- Todolist.py
+++ Todolist.py
@@ -1,9 +1,10 @@
 import time
 
 mylist = ["Do work", "Clean my room", "Eat something"]
+completelist = []
 
 print("Welcome to your To Do List!")
-time.sleep(1)
+time.sleep(0.3)
 
 print("Options\n1. Add a task\n2. Remove a task\n3. Complete a task\n4. Display tasks\n5. Exit")
 time.sleep(0.5)
@@ -13,16 +14,34 @@ while True:
 
   if A1 == "4":
      for i, x in enumerate(mylist, start=1):
-       print(f"{i}. {x}")
+       if x in completelist:
+         print(f"{i}. [X] {x}")
+       else:
+         print(f"{i}. [ ] {x}")
   elif A1 == "1":
     NW = input("Add a task: ")
     mylist.append(NW)
   elif A1 == "2":
     RW = int(input("Enter the number of the task to remove: ")) - 1
-    if 0 <= RW < len(mylist):
+    if 0 <= RW < len(mylist): 
       mylist.pop(RW)
-    else:
-      print("Invalid task number.")
+  elif A1 == "5":
+    print("Goodbye")
+    time.sleep(1)
+    break
+  elif A1 == "3":
+    print("Your to do list:")
+    for i, x in enumerate(mylist, start=1):
+     if x in completelist:
+        print(f"{i}. [X] {x}")
+     else:
+       print(f"{i}. [ ] {x}")
+    QW = int(input("Enter the number of the task you'd like to complete: ")) - 1
+    if 0 <= QW < len(mylist):
+      completelist.append(mylist[QW])
+
+  else:
+    print("Invalid task number.")
      
      
  
