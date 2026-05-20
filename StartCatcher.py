cf894a74bad145db95f470f80476cc5a32842e3a
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-05-20 22:00:16 +0000

    I started TicTacToe

--- StartCatcher.py
+++
@@ -1,6 +0,0 @@
-import pgzrun
-
-WIDTH = 300
-HEIGHT = 300
-
-pgzrun.go()
---
+++ TicTacToe.py
@@ -0,0 +1,9 @@
+class Game:
+    def __init__(self, score, turn, win, X,O, grid, position):
+        self.score = score
+        self.turn = turn
+        self.win = win
+        self.X = X
+        self.O = O 
+        self.grid = grid
+        self.position = position
