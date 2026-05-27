c303f6d11311af165b91fb4f10ca46303ea3b37d
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-05-27 21:57:47 +0000

    I did a side quest :)

---
+++ GuessingGame.py
@@ -0,0 +1,16 @@
+import random
+import time
+
+x = random.randint(0,9)
+
+guess_count = 0
+
+guess_limit = 3
+
+while True:
+    if guess_count < guess_limit:
+        current_guess = input("Guess a number betwee 0 and 9: ")
+        if current_guess = x
+            break
+        else 
+            
