7e3c23f2f01c7f834040bcffbec7b8b389abdc8b
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-06-05 17:24:11 +0000

    I started shopping cart

---
+++ ShoppingCart.py
@@ -0,0 +1,17 @@
+import time
+
+Cart ={
+
+}
+
+Stock = {
+    "Apple" : 0.75,     "Bread" : 4.50, 
+    "Carrot" : 0.50,
+    "Donut" : 1.25,
+    "Eggs" : 3.99,
+    "Flour" : 6.00,
+    "Grape" : 0.05,
+    "Honey" : 8.00,
+}
+
+print("Heres's what we have in stock: ", Stock)
