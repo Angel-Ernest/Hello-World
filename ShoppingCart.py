da0c889d9a4381474662985510f1b9461a8dcace
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-06-07 00:22:09 +0000

    I finished shopping cart

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -17,7 +17,7 @@ Stock = {
 
 print("Heres's what we have in stock: ")
 for key, value in Stock.items():
-    print(f"{key}: ${value}") #f allows to the code to replace the word that is in the brackets with the value of the variable
+    print(f"{key}    {value}") #f allows to the code to replace the word that is in the brackets with the value of the variable
 
 time.sleep(1)
 
@@ -27,5 +27,14 @@ while T:
     item = input("What would you like to buy? (Or type 'checkout' to finish) ")
     if item in Stock:
         Cart[item] = Stock[item] #Copy the item written to the other dictionary
+        for i, x in enumerate(Cart, start = 1): #i and x are not special variables, but they are used to count
+            print(f"{i}. {x}   {Cart[x]}")
+        print(f"Total:      {sum(Cart.values())}")
     elif item == "checkout":
         T = False
+        for i, x in enumerate(Cart, start = 1): #i and x are not special variables, but they are used to count
+            print(f"{i}. {x}   {Cart[x]}")
+        print(f"Your total is:      {sum(Cart.values())}")
+
+    else:
+        print("Sorry, we don't have that item in stock right now.")
