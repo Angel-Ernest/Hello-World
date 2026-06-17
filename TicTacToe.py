6026fe73d4f799d79f34f51793f62242fb0a26c8
Angel-Ernest <163440498+Angel-Ernest@users.noreply.github.com>
2026-06-17 22:07:54 +0000

    Last day :)

--- TicTacToe.py
+++ TicTacToe.py
@@ -7,26 +7,40 @@ class Game:
 
     def getTurn(self):
         return self.turn
-        
-        
+    
         (self.turn+"'s turn, enter the position:")
     
     def makeMove(self, turn, position):
         #Check if position is occupied 1. Check if variable position is present in self.position
         self.position.append(f"{turn}:{position}")
+     
         if turn == "X": # = means value, == means comparison value
             self.turn = "O"
         else:
             self.turn = "X"
 
-        print(turn)
+        #print(turn)
 
 G1 = Game(0, "X", [])
 
 while True:
     CurrentPlayer = G1.getTurn()
-    Position = input(f"{CurrentPlayer}'s turn: where do you want to play (select from 1 to 9)? ")
-    #Need to check the input (from 1 to 9) and can't play an existence playing
-    G1.makeMove(CurrentPlayer, Position)
+    Position = int(input(f"{CurrentPlayer}'s turn: where do you want to play (select from 1 to 9)? "))
 
-    print(G1.position)
+    if Position < 0 or Position > 9:
+        print("You can't play there.")
+        break
+
+    AlreadyPlay = False
+    
+    for x in G1.position:
+        L = x[-1]#Cut from the position; number indicates from where (-1 = last digit)
+        #print(L)
+        if L == Position:
+            AlreadyPlay = True
+            
+    if AlreadyPlay == False:
+        G1.makeMove(CurrentPlayer, Position)
+        print(f"{G1.turn} : {Position}")
+    else:
+        print("Number already occupied, please try again.")
